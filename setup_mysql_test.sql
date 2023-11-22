612cd603519ad730bc7d4707e7513e84640f83a1
kiplrry <kiplarry19@gmail.com>
2023-11-22 14:46:38 +0300

    hbnb_test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,5 +2,5 @@
 -- creates a db hbnb_test_db and user hbnb_test
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
