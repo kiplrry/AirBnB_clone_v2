d61f5eed5451fda2cfeb75ff7cad79389f7d36bc
kiplrry <kiplarry19@gmail.com>
2023-11-22 14:40:50 +0300

    hbnb_test_db

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- script that prepares a MySQL server for the test
+-- creates a user hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
