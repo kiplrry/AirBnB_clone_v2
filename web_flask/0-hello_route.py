3558c3ae55d918b7310c966392951408d5a2d9df
kiplrry <kiplarry19@gmail.com>
2024-02-17 01:19:34 +0300

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hbnb():
     """display “Hello HBNB!”"""
-    return "hbnb"
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
