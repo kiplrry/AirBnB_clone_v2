f2d8f9c826c6114856aaef692e670681155181bd
kiplrry <kiplarry19@gmail.com>
2024-02-17 01:26:46 +0300

    1-hbnb_route.py.fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,12 @@ def hbnb():
     """display “Hello HBNB!”"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb_too():
-    """display “HBNB!”"""
-    return "HBNB!"
+    """display “HBNB”"""
+    return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
