0f8a9eaca2a4b1240f2d942ea19f75ec0c740f88
kiplrry <kiplarry19@gmail.com>
2024-02-19 23:19:47 +0300

    4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Flask file"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hbnb():
+    """display “Hello HBNB!”"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_too():
+    """display “HBNB”"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """display “C ” followed by the value of the text
+    variable (replace underscore _ symbols with a space )"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/", defaults={'text': 'is cool'})
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """display “Python ” followed by the value of the text
+    variable (replace underscore _ symbols with a space )"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """display “n is a number” only if n is an integer"""
+    return f"{n} is number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
